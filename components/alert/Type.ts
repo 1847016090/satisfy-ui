7bea9142b6ec42f25c5b417fb1ddf407c2b5dc5d
Ye Kang <1847016090@qq.com>
2021-11-01 09:11:36 +0800

    refactor: 重构 UI

--- components/alert/Type.ts
+++
@@ -1,9 +0,0 @@
-export type Types = "info" | "positive" | "negative" | "warning";
-export type TypesMap = Record<Types, string>;
-export interface AlertProps {
-  /**
-   * Set this to change alert type
-   * @default info
-   */
-  type?: "info" | "positive" | "negative" | "warning";
-}
